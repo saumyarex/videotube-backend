e1261c1f861a37c14486a734f6fba347e3ca9f00
Saumya Nayak <saumyo224@gmail.com>
2025-05-24 18:25:46 +0530

    middleware inject in the register route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,19 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  //get data from front-end
+  //validation - not empty
+  //user already exist - by checking username and email
+  //check awatar given or not
+  //upload to cloudinary
+  //get cloudinary url
+  //create user object and create entry in db
+  //remove password and refresh token from response
+  //check user creation
+  //return res
+
+  const { fullName, username, email, password } = req.body;
+  console.log("Full Name : ", fullName, "Username : ", username);
 });
 
 export { registerUser };
