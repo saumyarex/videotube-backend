0a953613db1891592ba81ea2d079317553571df0
Saumya Nayak <saumyo224@gmail.com>
2025-05-24 16:12:16 +0530

    user controller and routes done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cookiesParser from 'cookies-parser';
-import cors from 'cors';
+import express from "express";
+import cookiesParser from "cookies-parser";
+import cors from "cors";
 
 const app = express();
 
@@ -11,9 +11,13 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: '16kb' }));
-app.use(express.urlencoded({ extended: true, limit: '16kb' }));
-app.use(express.static('public'));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(cookiesParser());
+
+// Routes import
+
+import userRouter from "./routes/user.routes.js";
 
 export { app };
