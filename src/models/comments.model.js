0e5a92c425a00e6436740622271c7abced1c57ea
Saumya Nayak <saumyo224@gmail.com>
2025-05-28 23:35:36 +0530

    comment model created

---
+++ src/models/comments.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const commentsSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Comment = mongoose.model("Comment", commentsSchema);
